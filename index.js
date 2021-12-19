ea3ec063722e3e90132e55ad6ab98ca90e461754
x3mator <x3mator@gmail.com>
2021-12-19 22:38:09 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const nameGood = 'булочка'
+const piecesGood = 21
+const category = 'булочные изделия'
+const price = 3
+
+console.log(`У нас есть ${piecesGood} ${nameGood} из категории "${category}", стоимостью ${price}$.`)
+console.log(`Общая сумма товара: ${piecesGood*price}$.`)
