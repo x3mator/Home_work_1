0dfa892a9e85cf1058854c0a79eaaf11aadb8d13
x3mator <x3mator@gmail.com>
2021-12-19 23:05:04 +0200

    Добавил второй товар

---
+++ lesson1_2/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+{
+  const nameGood = 'булочка'
+  const piecesGood = 21
+  const category = 'булочные изделия'
+  const price = 3
+
+  console.log(`У нас есть ${piecesGood} ${nameGood} из категории "${category}", стоимостью ${price}$.`)
+  console.log(`Общая сумма товара: ${piecesGood * price}$.`)
+}
+{
+  const nameGood = 'макароны'
+  const piecesGood = 5
+  const category = 'макаронные изделия'
+  const price = 4
+
+  console.log(`У нас есть ${piecesGood} ${nameGood} из категории "${category}", стоимостью ${price}$.`)
+  console.log(`Общая сумма товара: ${piecesGood * price}$.`)
+
+}
